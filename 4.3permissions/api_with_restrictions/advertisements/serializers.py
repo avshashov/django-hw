f1d6afd9cebf336777ef8179c2557358c60a5c2a
Artem Shashov <tauquantum@gmail.com>
2023-03-14 19:33:46 +1000

    added 4.3permissions

---
+++ 4.3permissions/api_with_restrictions/advertisements/serializers.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from advertisements.models import Advertisement
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer для пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name',
+                  'last_name',)
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    """Serializer для объявления."""
+
+    creator = UserSerializer(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Advertisement
+        fields = ('id', 'title', 'description', 'creator',
+                  'status', 'created_at',)
+
+    def create(self, validated_data):
+        """Метод для создания"""
+
+        # Простановка значения поля создатель по-умолчанию.
+        # Текущий пользователь является создателем объявления
+        # изменить или переопределить его через API нельзя.
+        # обратите внимание на `context` – он выставляется автоматически
+        # через методы ViewSet.
+        # само поле при этом объявляется как `read_only=True`
+        validated_data["creator"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+
+        if self.context["request"].method == 'POST' or (
+                self.context["request"].method == 'PATCH' and data.get('status') == 'OPEN'):
+            user_id = self.context["request"].user.pk
+            counter_status_open = Advertisement.objects.filter(creator=user_id, status='OPEN').count()
+            if counter_status_open >= 10:
+                raise ValidationError('Open advertisements can\'t be more than 10')
+
+        return data
